04ddf82f0012a1a85cfab676f20d405eeea30508
manjeetff <manjeet.agarwal@fiftyfivetech.io>
2024-05-10 20:20:22 +0530

    backend working tested

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,7 +13,7 @@ export const getAllUsers = async (req, res, next) => {
 export const getUserById = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const user = await UserService.getUserById(id);
+    const user = await userFunctions.getUserById(id);
     res.json(user);
   } catch (error) {
     next(error);
@@ -22,7 +22,7 @@ export const getUserById = async (req, res, next) => {
 
 export const createUser = async (req, res, next) => {
   try {
-    const newUser = await UserService.createUser(req.body);
+    const newUser = await userFunctions.createUser(req.body);
     res.status(201).json(newUser);
   } catch (error) {
     next(error);
@@ -32,7 +32,7 @@ export const createUser = async (req, res, next) => {
 export const updateUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const updatedUser = await UserService.updateUser(id, req.body);
+    const updatedUser = await userFunctions.updateUser(id, req.body);
     res.json(updatedUser);
   } catch (error) {
     next(error);
@@ -42,7 +42,7 @@ export const updateUser = async (req, res, next) => {
 export const deleteUser = async (req, res, next) => {
   try {
     const { id } = req.params;
-    await UserService.deleteUser(id);
+    await userFunctions.deleteUser(id);
     res.sendStatus(204);
   } catch (error) {
     next(error);
